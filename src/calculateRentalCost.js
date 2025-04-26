e902a34bc56e5051af23d3d082fa63ab451b0c97
antmen71 <orkunantmen@hotmail.com>
2025-04-26 17:08:31 +0300

    calculateRentalCost first push

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,26 @@
  *
  * @return {number}
  */
+
 function calculateRentalCost(days) {
-  // write code here
+  const dayRental = 40;
+  const sevenDaysOff = 50;
+  const threeDaysOff = 20;
+
+  if (days < 3) {
+    // eslint-disable-next-line no-const-assign
+    return days * dayRental;
+  }
+
+  if (days >= 3 && days <= 6) {
+    // eslint-disable-next-line no-const-assign
+    return days * dayRental - threeDaysOff;
+  }
+
+  if (days >= 7) {
+    // eslint-disable-next-line no-const-assign
+    return days * dayRental - sevenDaysOff;
+  }
 }
 
 module.exports = calculateRentalCost;
